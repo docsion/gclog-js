ed6101d75b29ff24f550421b1e2e4a74a537913c
Rye Nguyen <ryenguyen7411@gmail.com>
2022-05-23 14:39:45 +0700

    build: setup devtools

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+const automaticCommitPattern = /^chore\(release\):.*\[skip ci]/;
+
+module.exports = {
+  extends: [
+    '@commitlint/config-conventional',
+  ],
+  ignores: [
+    commitMsg => automaticCommitPattern.test(commitMsg),
+  ],
+};
